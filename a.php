8c5a62f5f8fd84d2694ab00e13bd84845d3a99e4
j341nono <kanakana.nono1224@gmail.com>
2024-06-24 13:51:47 +0900

    Merge changes from origin/main

---
+++ a.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+foreach (file('/usr/share/dict/words') as $line) {
+    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
+    if (strpos($line, 'abc') !== false) {
+        echo "$line";
+    }
+}
+?>
