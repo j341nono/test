1294d2b88f18721224ad870b731e0f584875225b
j341nono <kanakana.nono1224@gmail.com>
2024-06-25 16:17:07 +0900

    文字列を数値に変換する処理がないため、本来は数値の加算をするところが文字列の結合になっている

--- a.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-foreach (file('/usr/share/dict/words') as $line) {
-    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-    if (strpos($line, 'abc') !== false) {
-        echo "$line";
-    }
-}
-?>
